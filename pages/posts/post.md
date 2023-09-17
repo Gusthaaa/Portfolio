403b89b6b989841b57267bdb274740e41e969d74
Gustavo Hatanaka <gustavohatanaka@edu.unifil.br>
2023-09-17 17:50:39 -0300

    first-commit

---
+++ pages/posts/post.md
@@ -0,0 +1,9 @@
+---
+title: First Post
+date: 15/09/2023
+description: This is my first post/portfolio
+tag: web development
+author: You
+---
+
+This is my first post.
--- theme.config.js
+++ theme.config.js
@@ -3,8 +3,8 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
-      <a href="/feed.xml">RSS</a>
+      <time>{YEAR}</time> Gustavo T. Hatanaka.
+      <a href="http://localhost:3000/">Home Page</a>
       <style jsx>{`
         a {
           float: right;
